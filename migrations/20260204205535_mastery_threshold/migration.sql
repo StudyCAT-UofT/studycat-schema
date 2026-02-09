6de43ff5123ae87d4a31387c0da8b6e7bd088ec5
Jayden Chiola-Nakai <74999083+jchionak@users.noreply.github.com>
2026-02-09 09:22:08 -0500

    Added feedbackVisibility and masteryThreshold columns (#7)
    feedbackVisibility added to Quiz and masteryThreshold added to QuizModule.

---
+++ migrations/20260204205535_mastery_threshold/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `masteryThreshold` to the `QuizModule` table without a default value. This is not possible if the table is not empty.
+
+*/
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[QuizModule] ADD [masteryThreshold] FLOAT(53) NOT NULL;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
