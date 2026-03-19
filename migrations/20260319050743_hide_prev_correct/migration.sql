0d2ab92f13ef49b1bea5ca7d1c958fab75dcf778
jchionak <jchionak@icloud.com>
2026-03-19 01:09:51 -0400

    add repeat correct questions to Quiz

---
+++ migrations/20260319050743_hide_prev_correct/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Quiz] ADD [repeatCorrectQuestions] BIT NOT NULL CONSTRAINT [Quiz_repeatCorrectQuestions_df] DEFAULT 1;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
