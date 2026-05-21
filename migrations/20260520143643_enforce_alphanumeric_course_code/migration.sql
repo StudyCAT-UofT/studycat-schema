bf45f2d8e4572bc3926f1218a5e86662add1723b
elena-ding <emailelenading@gmail.com>
2026-05-21 11:01:52 -0400

    add try/catch transaction block

--- migrations/20260520143643_enforce_alphanumeric_course_code/migration.sql
+++ migrations/20260520143643_enforce_alphanumeric_course_code/migration.sql
@@ -1,1 +1,18 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
 ALTER TABLE [dbo].[Course] ADD CONSTRAINT [check_course_code_alphanumeric] CHECK ([code] NOT LIKE '%[^a-zA-Z0-9]%');
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
